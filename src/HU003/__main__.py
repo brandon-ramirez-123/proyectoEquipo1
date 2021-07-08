a19ffb0e2608d0a99aad132fa3068684c2e0f04e
brandon-ramirez-123 <bv777bv777@gmail.com>
2021-07-08 15:36:31 +0200

    Versión 0.05b: Soporte parcial para pantallas de alta resolución con un escalado mayor a 100% e inicio de soporte para Windows 11

--- src/HU003/__main__.py
+++ src/HU003/__main__.py
@@ -1,8 +1,15 @@
 import sys
+
+from PyQt5 import QtCore, QtWidgets
+
 from logica.hu003 import uiEstudiantes
 from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
     app = QApplication(sys.argv)
     dialogo = uiEstudiantes()
     dialogo.show()
